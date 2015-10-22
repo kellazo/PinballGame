a9abb9bff4ced1b561d42007ecf1025e615ff863
Pau5erra <pau.serra11@hotmail.com>
2015-10-22 22:31:15 +0200

    Foto i pantalla bona
    sdf

--- Pinball/Globals.h
+++ Pinball/Globals.h
@@ -21,7 +21,7 @@ enum update_status
 };
 
 // Configuration -----------
-#define SCREEN_WIDTH 573
+#define SCREEN_WIDTH 826
 #define SCREEN_HEIGHT 831
 #define SCREEN_SIZE 1
 #define WIN_FULLSCREEN false
